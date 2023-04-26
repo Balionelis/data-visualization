8d1085a93799230c21e35b8d96b1dff41498db4d
Blijonas <blijonasq@gmail.com>
2023-04-26 13:22:54 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import mpld3
+import webbrowser
+
+# Name of the .html file
+filename = 'scatter_plot.html'
 
 # Create some random data
-x = np.random.rand(50)
-y = np.random.rand(50)
+x = np.random.rand(10)
+y = np.random.rand(10)
+indices = np.arange(10)  # Define an array of indices
 
 # Create a scatter plot with different colors and sizes for each point
-colors = np.random.rand(50)
-sizes = 1000 * np.random.rand(50)
+colors = np.random.rand(10)
+sizes = 1000 * np.random.rand(10)
 plt.scatter(x, y, c=colors, s=sizes, alpha=0.5)
 
 # Add a colorbar to show the range of colors
@@ -19,10 +25,13 @@ plt.title("Random Scatter Plot")
 plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 
-# Add a tooltip with the coordinates of each point
-tooltip = mpld3.plugins.PointLabelTooltip(plt.scatter(x, y))
+# Add a tooltip with the coordinates and indices of each point
+tooltip_text = ["x: {:0.2f}, y: {:0.2f}, index: {}".format(x[i], y[i], indices[i]) for i in range(len(x))]
+tooltip = mpld3.plugins.PointHTMLTooltip(plt.scatter(x, y, c=colors, s=sizes, alpha=0.5), tooltip_text)
 mpld3.plugins.connect(plt.gcf(), tooltip)
 
 # Save the plot as an HTML file
 mpld3.save_html(plt.gcf(), "scatter_plot.html")
 
+# Automatically opens the file
+webbrowser.open_new_tab(filename)
